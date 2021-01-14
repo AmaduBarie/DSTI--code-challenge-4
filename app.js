ab86fd9bb729b3bf986fb381a8f5cfc517c4b57d
Amadu Barrie <slsales.me@gmail.com>
2021-01-14 04:35:38 +0000

    css fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //setup public folder
-app.use(express.static('./images'));
+app.use(express.static('./Public'));
 app.get('/',function (req, res) {    
 res.render('index.ejs')
 });
